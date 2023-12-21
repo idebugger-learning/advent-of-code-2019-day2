5cfd31f0f38d3745538b081adff0780c72af3b16
iDeBugger Freeman <me@idebugger.io>
2023-12-21 22:55:56 +0100

    Part 2

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,5 @@
 pub struct CPU {
-    program: Vec<isize>,
+    memory: Vec<isize>,
     ip: usize,
     halted: bool,
 }
@@ -7,24 +7,25 @@ pub struct CPU {
 impl CPU {
     pub fn new(program: Vec<isize>) -> Self {
         CPU {
-            program,
+            memory: program,
             ip: 0,
             halted: false,
         }
     }
 
-    pub fn get_first_value(&self) -> isize {
-        self.program[0]
-    }
+    pub fn run(&mut self, param1: isize, param2: isize) -> isize {
+        self.memory[1] = param1;
+        self.memory[2] = param2;
 
-    pub fn run(&mut self) {
         while !self.halted {
             self.step();
         }
+
+        self.memory[0]
     }
 
     fn step(&mut self) {
-        match self.program[self.ip] {
+        match self.memory[self.ip] {
             1 => self.step_add(),
             2 => self.step_mul(),
             99 => self.step_halt(),
@@ -33,21 +34,21 @@ impl CPU {
     }
 
     fn step_add(&mut self) {
-        let operand1 = self.program[self.program[self.ip + 1] as usize];
-        let operand2 = self.program[self.program[self.ip + 2] as usize];
+        let operand1 = self.memory[self.memory[self.ip + 1] as usize];
+        let operand2 = self.memory[self.memory[self.ip + 2] as usize];
 
-        let target_position = self.program[self.ip + 3] as usize;
-        self.program[target_position] = operand1 + operand2;
+        let target_position = self.memory[self.ip + 3] as usize;
+        self.memory[target_position] = operand1 + operand2;
 
         self.ip += 4;
     }
 
     fn step_mul(&mut self) {
-        let operand1 = self.program[self.program[self.ip + 1] as usize];
-        let operand2 = self.program[self.program[self.ip + 2] as usize];
+        let operand1 = self.memory[self.memory[self.ip + 1] as usize];
+        let operand2 = self.memory[self.memory[self.ip + 2] as usize];
 
-        let target_position = self.program[self.ip + 3] as usize;
-        self.program[target_position] = operand1 * operand2;
+        let target_position = self.memory[self.ip + 3] as usize;
+        self.memory[target_position] = operand1 * operand2;
 
         self.ip += 4;
     }
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,31 @@ mod cpu;
 fn main() {
     // let input = include_str!("../data/example1.txt");
     let input = include_str!("../data/input.txt");
-    let mut input = input
+    let input = input
         .split(',')
         .map(|v| v.parse::<isize>().unwrap())
         .collect::<Vec<_>>();
 
-    input[1] = 12;
-    input[2] = 2;
+    let expected_result = 19690720;
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            print!("Trying noun {} and verb {}: ", noun, verb);
 
-    let mut cpu = CPU::new(input);
-    cpu.run();
+            let mut cpu = CPU::new(input.clone());
+            let result = cpu.run(noun, verb);
 
-    println!("First value after run: {}", cpu.get_first_value());
+            print!("{}. ", result);
+            if result == expected_result {
+                println!("✅ Match!");
+                print_result(noun, verb);
+                return;
+            } else {
+                println!("Nah.")
+            }
+        }
+    }
+}
+
+fn print_result(noun: isize, verb: isize) {
+    println!("The answer is {}", 100 * noun + verb)
 }
